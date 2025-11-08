7feb553756898d73602f1f18ba03bb037f4f35a4
ben <benjamin.surfer14@gmail.com>
2025-11-08 14:11:01 -0700

    made osc much faster

--- src/Oscillator.h
+++ src/Oscillator.h
@@ -23,7 +23,6 @@ typedef enum Waveform
 typedef struct Oscillator
 {
   Waveform waveform;
-  R4 (*waveformFunc)(R4,R4);
   R4 phase;
   R4 pulsWidth;
   R4 stride;
@@ -40,11 +39,6 @@ void Oscillator_free(Oscillator * osc);
 
 void Oscillator_setFreq(Oscillator * osc, R4 freq);
 void Oscillator_setPW(Oscillator * osc, R4 pw);
-void Oscillator_setWaveform(Oscillator * osc, Waveform waveform);
-void Oscillator_sampleAndUpdate(Oscillator * osc, R4 * samples, U4 samplesSize);
-void Oscillator_sampleAndUpdateFreqSweep(Oscillator * osc, R4 * samples, U4 samplesSize, R4 startFreq, R4 endFreq);
-void Oscillator_sampleAndUpdateStrideTable(Oscillator * osc, R4 * samples, U4 samplesSize, R4 * strideTable);
 void Oscillator_sampleWithStrideAndPWTable(Oscillator * osc, R4 * samples, U4 samplesSize, R4 * strideTable, R4 * pwTable);
-R4 Oscillator_singleSampleAndUpdate(Oscillator * osc);
 
 #endif
