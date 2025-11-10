32a57129667a5ac3fb0e4b0a433434f783788afb
Benjamin Clark <benjamin.surfer14@gmail.com>
2025-11-09 20:44:21 -0700

    midi defs

---
+++ src/MIDI.h
@@ -0,0 +1,33 @@
+#ifndef MIDI_H_
+#define MIDI_H_
+
+
+#include "comm/Common.h"
+#include "Module.h"
+
+///////////////
+//  DEFINES  //
+///////////////
+
+#define MAX_NOTES   5
+
+///////////
+// TYPES //
+///////////
+
+typedef U2 MIDIPortID;
+
+typedef struct MIDIUpdate
+{
+    R4 note[MAX_NOTES];
+    bool noteIsOn[MAX_NOTES];
+    bool noteIsNewlyOn[MAX_NOTES];
+    R4 velocity[MAX_NOTES];
+} MIDIUpdate;
+
+////////////////////////
+//  PUBLIC FUNCTIONS  //
+////////////////////////
+
+
+#endif
